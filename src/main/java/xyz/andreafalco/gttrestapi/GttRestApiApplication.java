fbb18639e962b79dace6082e71325e1e545b73f4
Andrea Falco <an.falco@reply.it>
2023-05-24 21:45:00 +0200

    Initial commit

---
+++ src/main/java/xyz/andreafalco/gttrestapi/GttRestApiApplication.java
@@ -0,0 +1,13 @@
+package xyz.andreafalco.gttrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GttRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GttRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/andreafalco/gttrestapi/GttRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.andreafalco.gttrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GttRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
