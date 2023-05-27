814be2daa09957f6c724689337bf6a1bc6d0df8c
Andrea Falco <andryxx_96@hotmail.it>
2023-05-27 18:57:32 +0200

    Add a property to enable/disable request logging

--- src/main/java/xyz/andreafalco/gttrestapi/controller/GttApiController.java
+++ src/main/java/xyz/andreafalco/gttrestapi/controller/GttApiController.java
@@ -1,5 +1,6 @@
 package xyz.andreafalco.gttrestapi.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,9 @@ import java.util.Objects;
 @RestController
 @RequestMapping("gtt")
 public class GttApiController {
+
+    @Value("${gtt.api.log:false}")
+    private Boolean log;
 
     private final GttService gttService;
     private final GttLogService gttLogService;
@@ -30,7 +34,7 @@ public class GttApiController {
         StopTimetables stopTimetables = gttService.gtt(stop);
 
         // Log stop timetables
-        if (Objects.nonNull(stopTimetables)) {
+        if (log && Objects.nonNull(stopTimetables)) {
             gttLogService.gttLog(stopTimetables);
         }
 
