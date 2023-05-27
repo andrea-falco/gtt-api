90027c816f3e52541e8c503a3f4e5217738d704d
Andrea Falco <an.falco@reply.it>
2023-05-27 18:35:55 +0200

    Add logging logic to REST controller

--- src/main/java/xyz/andreafalco/gttrestapi/controller/GttApiController.java
+++ src/main/java/xyz/andreafalco/gttrestapi/controller/GttApiController.java
@@ -6,21 +6,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.andreafalco.gttrestapi.model.dto.StopTimetables;
 import xyz.andreafalco.gttrestapi.model.response.StopResponse;
+import xyz.andreafalco.gttrestapi.service.GttLogService;
 import xyz.andreafalco.gttrestapi.service.GttService;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("gtt")
 public class GttApiController {
 
     private final GttService gttService;
+    private final GttLogService gttLogService;
 
-    public GttApiController(GttService gttService) {
+    public GttApiController(GttService gttService, GttLogService gttLogService) {
         this.gttService = gttService;
+        this.gttLogService = gttLogService;
     }
 
     @GetMapping("/{stop}")
     public StopResponse timetablesByStop(@PathVariable String stop) {
+
+        // Get stop timetables
         StopTimetables stopTimetables = gttService.gtt(stop);
+
+        // Log stop timetables
+        if (Objects.nonNull(stopTimetables)) {
+            gttLogService.gttLog(stopTimetables);
+        }
+
         return new StopResponse(stopTimetables);
     }
 
