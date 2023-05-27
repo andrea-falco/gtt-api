97c1314ee9145205cf61e6ed95013085c056162c
Andrea Falco <an.falco@reply.it>
2023-05-27 18:34:29 +0200

    Add GttLogService interface

---
+++ src/main/java/xyz/andreafalco/gttrestapi/service/GttLogService.java
@@ -0,0 +1,9 @@
+package xyz.andreafalco.gttrestapi.service;
+
+import xyz.andreafalco.gttrestapi.model.dto.StopTimetables;
+
+public interface GttLogService {
+
+    void gttLog(StopTimetables gtt);
+
+}
